c07f422e9b3820533e123df1d10558419d0d7e36
Priya sinha <48295604+Oprishri@users.noreply.github.com>
2020-10-15 22:37:26 +0530

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,3 +1,15 @@
-# Data-Structure-Sorting-Techniques-
-/
-Insertion Sort.py
+# DataStructure (Sorting-Techniques)-Insertion Sort
+
+arr = [64, 25, 12, 22, 11]
+
+def insertion_sort(arr):
+  for i in range(1,len(arr)):
+    key = arr[i]
+    j=i-1
+    while j>=0 and key<arr[j]: 
+      arr[j+1]=arr[j]
+      j-=1
+    arr[j+1]=key
+  return arr
+
+insertion_sort(arr)
